25a0fbb7db451da2db6c934071e01a1af6f7c004
brunocduran <brunocesarduran@gmail.com>
2024-02-08 22:34:17 -0300

    custom listener

--- str-consumer/src/main/java/br/com/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/br/com/strconsumer/listeners/StrConsumerListener.java
@@ -1,15 +1,51 @@
 package br.com.strconsumer.listeners;
 
+import br.com.strconsumer.custom.StrConsumerCustomListner;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    //Configuracao antes de criar a anotacao personalizada
+    /*@KafkaListener(groupId = "group-1",
+            topicPartitions = {
+                @TopicPartition(topic = "str-topic", partitions = {"0"})
+            },
+            containerFactory = "strContainerFactory")
     public void create(String message){
-        log.info("Receive message {}", message);
+        log.info("CREATE ::: Receive message {}", message);
+    }
+
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = {
+                    @TopicPartition(topic = "str-topic", partitions = {"1"})
+            },
+            containerFactory = "strContainerFactory")
+    public void log(String message){
+        log.info("LOG ::: Receive message {}", message);
+    }
+
+    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void history(String message){
+        log.info("HISTORY ::: Receive message {}", message);
+    }*/
+
+    @StrConsumerCustomListner(groupId = "group-1")
+    public void create(String message){
+        log.info("CREATE ::: Receive message {}", message);
+    }
+
+    @StrConsumerCustomListner(groupId = "group-1")
+    public void log(String message){
+        log.info("LOG ::: Receive message {}", message);
+    }
+
+    @StrConsumerCustomListner(groupId = "group-2")
+    public void history(String message){
+        log.info("HISTORY ::: Receive message {}", message);
     }
 }

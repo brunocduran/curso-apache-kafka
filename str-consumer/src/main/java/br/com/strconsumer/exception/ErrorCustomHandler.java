d38ea66a3236adf8bcbde58952f7dbb79e959bb5
brunocduran <brunocesarduran@gmail.com>
2024-02-08 23:16:47 -0300

    error handler 2

--- str-consumer/src/main/java/br/com/strconsumer/exception/ErrorCustomHandler.java
+++ str-consumer/src/main/java/br/com/strconsumer/exception/ErrorCustomHandler.java
@@ -1,10 +1,9 @@
 package br.com.strconsumer.exception;
 
 import lombok.extern.log4j.Log4j2;
-import org.apache.kafka.clients.consumer.Consumer;
+
 import org.springframework.kafka.listener.KafkaListenerErrorHandler;
 import org.springframework.kafka.listener.ListenerExecutionFailedException;
-import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +13,10 @@ public class ErrorCustomHandler implements KafkaListenerErrorHandler {
     @Override
     public Object handleError(Message<?> message, ListenerExecutionFailedException exception) {
         log.info("EXCEPTION_HANDLER ::: Capturei um erro");
+        log.info("Payload ::: `{}", message.getPayload());
+        log.info("Headers ::: `{}", message.getHeaders());
+        log.info("Offset ::: `{}", message.getHeaders().get("kafka_offset"));
+        log.info("Message exception ::: `{}", exception.getMessage());
         return null;
     }
 
